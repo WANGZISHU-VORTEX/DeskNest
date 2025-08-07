9c2b3aeb67948bb5e8222c081209acf32522cb03
root <root@rhel.vortex>
2025-08-08 00:54:39 +0900

    Init DestNest Project

---
+++ src/module/database.module.ts
@@ -0,0 +1,32 @@
+// src/module/database.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { TestEntity } from '../../test/test.entity';
+import { config } from 'process';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [TestEntity],
+        synchronize: configService.get<string>('NODE_ENV') === 'test',
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {
+  static forFeature(entities: any[]) {
+    return TypeOrmModule.forFeature(entities);
+  }
+}
